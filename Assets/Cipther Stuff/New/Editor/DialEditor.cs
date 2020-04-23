c9c424f96e3886dca4b9fcb2a1461cca1b60d7e3
Khejanin <khejanin@gmail.com>
2020-04-23 11:17:53 +0200

    More Markers and a Tracking script

---
+++ Assets/Cipther Stuff/New/Editor/DialEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Dial))]
+[CanEditMultipleObjects]
+public class DialEditor : Editor
+{
+    Dial _target;
+    
+    void OnEnable()
+    {
+        _target = (Dial) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Up"))
+        {
+            _target.Up();
+        }
+        
+        else if (GUILayout.Button("Down"))
+        {
+            _target.Down();
+        }
+
+        
+    }
+}
